abb09c1c0d5b1e7ae8f69be2836c98247ab05ce7
mattcroberts <mattroberts.uk@gmail.com>
2016-02-21 01:54:32 +0000

    further webpack config

--- server.js
+++ server.js
@@ -2,8 +2,33 @@ import express from 'express';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Index from './components/Index';
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpack from 'webpack';
 
 const app = express();
+
+const compiler = webpack({
+  entry: './components/Index.jsx',
+  output: {
+    path: '/'
+  },
+  module: {
+      loaders: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react']
+          }
+        }
+      ]
+    }
+});
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: '/public/'
+}));
 
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
